28d0fab2d5cbf524d0f313eb9009a2037eb7c873
Filip Alberski <alberskif@gmail.com>
2023-12-01 13:23:40 +0100

    Add marketing page content

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -9,6 +9,13 @@ const MarketingPage = () => {
           <Medal className="h-6 w-6 mr-3" />
           No 1 task management app
         </div>
+        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
+          Trello helps team move
+        </h1>
+        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4  p-2 rounded-md pb-4 w-fit">
+          Work forward in Trello with your team. A collaborative,
+          visual way to organize teamwork in any business.
+        </div>
       </div>
     </div>
   );
